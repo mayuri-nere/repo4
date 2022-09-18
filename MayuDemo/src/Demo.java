1ad4dbc2fafe235cf8aa255200b1641750872740
mayuri-nere <mayuri.nere96@gmail.com>
2022-09-18 15:40:55 +0530

    This Mayu Demo Contains the 1st Demo for repository

---
+++ MayuDemo/src/Demo.java
@@ -0,0 +1,40 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float f=5.5f;
+		boolean flag=true;
+		boolean flag1=false;
+System.out.println("My fist project "  +" " +   "welcome new city");
+System.out.println("First" + " " + " second" + " "+ "Third" +" "+ "\nfourth");
+System.out.println("");
+System.out.println("\n" + " " + "\n\nNew");
+System.out.println("New");
+System.out.println("\n \n happy");
+System.out.println(f);
+System.out.println(flag1);
+System.out.println(flag);
+int a=1, b=15;
+int c=a + b;
+System.out.println(c);
+ c = a + b;
+
+	}
+	class mayuri {
+		mayuri(){
+			
+		}
+	public void add()
+	{
+		
+	}
+		
+		
+	}
+	
+	
+
+}
+
+
